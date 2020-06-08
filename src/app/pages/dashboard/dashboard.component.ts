16d50d0972e33d50dddc5751763fb9a2ed10f9d8
Brian Froschauer <bafroschauer@gmail.com>
2020-06-08 00:56:53 -0300

    group data

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { ChartConfiguration } from 'src/model/line-area/ChartConfiguration';
 import { BathroomData } from 'src/model/line-area/bathroom/BathroomData';
 import { ZoneData } from 'src/model/bar-chart/zone/ZoneData';
 import { AveragePriceData } from 'src/model/bar-chart/average-price/AveragePriceData';
+import {CoveredSurfaceItem} from "../../../model/line-area/covered/CoveredSurfaceItem";
 
 @Component({
   selector: 'app-dashboard',
@@ -33,6 +34,27 @@ export class DashboardComponent implements OnInit {
   constructor(private dashboardService: DashboardService) {
 
     this.dashboardService.getCoveredSurface().subscribe(data => {
+
+      data.map(item => {
+        item.covered_surface <= 250 ? item.covered_surface = 250 :
+        item.covered_surface <= 500 ? item.covered_surface = 500 :
+        item.covered_surface <= 750 ? item.covered_surface = 750 :
+        item.covered_surface <= 1000 ? item.covered_surface = 1000 :
+        item.covered_surface <= 1250 ? item.covered_surface = 1250 :
+        item.covered_surface = 1500
+      });
+
+      let group = data.reduce((r, a) => {
+        r[a.covered_surface] = [...r[a.covered_surface] || [], a];
+        return r;
+      }, {});
+
+      Object
+        .keys(group)
+        .forEach(key => group[key] = { covered_surface: `[${Number.parseInt(key) - 250}, ${key}]`, average_price: getAveragePrice(group[key]) })
+
+      data = Object.values(group);
+      console.log(data);
       this.coveredSurface = new CoveredSurfaceData(data);
     });
 
@@ -56,7 +78,12 @@ export class DashboardComponent implements OnInit {
     this.dashboardService.getAveragePriceByZone("desc", 5).subscribe(data => {
       this.afforadableAverage = new AveragePriceData(data);
     });
-    
+
+    const getAveragePrice = (items: CoveredSurfaceItem[]) => {
+      let sum = 0;
+      items.forEach(item => sum += item.average_price);
+      return Math.ceil(sum / items.length);
+    }
   }
 
   ngOnInit(): void {
